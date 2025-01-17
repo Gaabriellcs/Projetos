24ed5c42eecbe6191c4b1224b240fad7aa3251ee
ezequiel.souza <ezequiel.souza@gruposantamartadf.com.br>
2025-01-17 09:30:06 -0300

    vai

--- Controle De Gasto/back/ControleDeGasto/ControleDeGasto/obj/Release/net8.0/ControleDeGasto.AssemblyInfo.cs
+++ Controle De Gasto/back/ControleDeGasto/ControleDeGasto/obj/Release/net8.0/ControleDeGasto.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeGasto")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd26ed6240450a755f980cbf86d427f120166808")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeGasto")]
